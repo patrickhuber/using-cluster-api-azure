1d842210e47ede0d8a056050fde6024c68b581af
patrickhuber <patrickhuber@users.noreply.github.com>
2024-10-07 10:14:19 -0400

    bump version

--- .devcontainer/local-features/clusterctl/install.sh
+++ .devcontainer/local-features/clusterctl/install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
-curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.7.2/clusterctl-linux-amd64 -o clusterctl
+curl -L https://github.com/kubernetes-sigs/cluster-api/releases/download/v1.8.3/clusterctl-linux-amd64 -o clusterctl
 chmod +x clusterctl
 cp clusterctl /usr/local/bin
+rm clusterctl
